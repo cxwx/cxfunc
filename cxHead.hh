52544be27e18a4681e1cd9ee17d0b82ac4db4609
cx <6235641+cxwx@users.noreply.github.com>
2026-06-03 13:00:44 +0800

    add include header for clangd

---
+++ cxHead.hh
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TGraph.h>
+#include <TGraphAsymmErrors.h>
+#include <TGraphErrors.h>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TH1I.h>
+#include <TKey.h>
+#include <TMath.h>
+#include <TMatrix.h>
+#include <TMatrixD.h>
+#include <TMatrixF.h>
+#include <TMinuit.h>
+#include <TMinuitMinimizer.h>
+#include <TObject.h>
+#include <TProfile.h>
+#include <TROOT.h>
+#include <TString.h>
+#include <TStyle.h>
+#include <TTree.h>
+#include <TVector.h>
+#include <TVectorD.h>
+#include <TVectorF.h>
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
