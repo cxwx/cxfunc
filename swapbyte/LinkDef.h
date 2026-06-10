f7c15177465c4edfef1f08b07f3404c6cf14afb3
cx <6235641+cxwx@users.noreply.github.com>
2026-06-10 20:59:33 +0800

    fix namespace ROOT
    fix

--- swapbyte/LinkDef.h
+++ swapbyte/LinkDef.h
@@ -1,9 +1,8 @@
-#ifdef __CINT__
+#ifdef __CLING__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ namespace cxfunc::type;
 #pragma link C++ class cxfunc::type::short2bit+;
 #pragma link C++ function cxfunc::swapbyte::swapsOld;
 
